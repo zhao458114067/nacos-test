c5c06d3871d77f142a6e21d1200fd18e97828e6c
zhao458114067 <458114067@qq.com>
2021-02-07 22:27:26 +0800

    初始化

---
+++ nacos-consumer/src/main/java/com/zx/ConsumerApplication.java
@@ -0,0 +1,17 @@
+package com.zx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Nacos starter.
+ *
+ * @author zhaoxu
+ */
+@SpringBootApplication
+public class ConsumerApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
+}
